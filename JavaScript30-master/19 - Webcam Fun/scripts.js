57a8b487d65295f225f519e20b5d95022807b409
Siddarth Menon <33069694+s1ddarth@users.noreply.github.com>
2025-03-27 17:14:37 +0530

    Day 19: Wrote the 3 functions to modify either the red values, create an rgb split or to edit the colors using the green screen function

--- JavaScript30-master/19 - Webcam Fun/scripts.js
+++ JavaScript30-master/19 - Webcam Fun/scripts.js
@@ -30,7 +30,15 @@ function paintToCanvas() {
   // Run the function on an interval
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
-    console.log("Drawing image to canvas");
+    let pixels = ctx.getImageData(0, 0, width, height);
+    
+    // Running image effects
+    // pixels = redEffect(pixels);
+    pixels = rgbSplit(pixels);
+    // pixels = greenScreen(pixels);
+
+    // Putting the edited image back on the canvas
+    ctx.putImageData(pixels, 0, 0);
   }, updateInterval);
 }
 
@@ -45,6 +53,52 @@ function takePhoto() {
   link.setAttribute("download", "webcamImage");
   link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
   strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 200; // Red
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; // Green
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 0]; // Red
+    pixels.data[i + 500] = pixels.data[i + 1]; // Green
+    pixels.data[i - 550] = pixels.data[i + 2]; // Blue
+  }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll(".rgb input").forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (
+      red >= levels.rmin &&
+      green >= levels.gmin &&
+      blue >= levels.bmin &&
+      red <= levels.rmax &&
+      green <= levels.gmax &&
+      blue <= levels.bmax
+    ) {
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
 }
 
 getVideo();
